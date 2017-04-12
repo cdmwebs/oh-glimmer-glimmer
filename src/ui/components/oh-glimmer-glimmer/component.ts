6bc6d0e327fad7166579962a814bd1fc6bb13d22
Ruslan Zavacky <ruslan.zavacky@crowdstrike.com>
2017-04-12 10:44:07 +0100

    Pass variables and draw initial container

--- src/ui/components/oh-glimmer-glimmer/component.ts
+++ src/ui/components/oh-glimmer-glimmer/component.ts
@@ -1,5 +1,6 @@
 import Component from "@glimmer/component";
 
 export default class OhGlimmerGlimmer extends Component {
-
+  days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'];
 }
